643f83727191a871bb588f1ea471a9d53905f1d3
phucpham <phamhuuphuc4049@gmail.com>
2024-11-24 15:52:05 +0700

    fix tailwind error

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./pages/Layout";
+import './index.css'
 // import Home from "./pages/Home";
 // import Blogs from "./pages/Blogs";
 // import Contact from "./pages/Contact";
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Homepage = () => {
   return (
-    <div>
+    <div className='bg-blue-200'>
       Homepage
     </div>
   )
